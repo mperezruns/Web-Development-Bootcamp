b5801e298c209226a520f82f715853b09c15ad45
Mark Perez <marcosperez@cpp.edu>
2022-08-29 11:19:17 -0700

    first simple war commit and milestone p2

--- wu_project.py
+++ wu_project.py
@@ -1,12 +1,13 @@
+import random
 # SINGLE CARD CLASS
 # CREATING A CARD CLASS WITH OUTSIDE VARIABLES
 # SUIT, RANK, VALUES
-import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
             'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
+# DEFINING A CLASS FOR THE CARDS
 class Card:
 
     def __init__(self, suit, rank):
@@ -31,7 +32,7 @@ print(two_hearts)
 class Deck:
 
     def __init__(self):
-        # THis only happens once upon creation of a new Deck
+        # This only happens once upon creation of a new Deck
         self.all_cards = []
         for suit in suits:
             for rank in ranks:
@@ -46,6 +47,7 @@ class Deck:
         # This remove one card from the list of all_cards
         return self.all_cards.pop()
 
+# CREATING THE 52 DECK OF CARDS
 mydeck = Deck()
 print(len(mydeck.all_cards))
 
@@ -75,7 +77,7 @@ class Player:
 
     def add_cards(self, new_cards):
         if type(new_cards) == type([]):
-            self.all_cards().extend(new_cards)
+            self.all_cards.extend(new_cards)
         else:
             self.all_cards.append(new_cards)
 
@@ -83,13 +85,36 @@ class Player:
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
 
+# DEFINING MARK AS A PLAYER
 mark = Player("Mark")
 mark
 
+# THE OUTPUT RETURNS THE NUMBER OF CARDS GIVEN TO THE PLAYER
 print(mark)
-
+# MARK HAS A CARD
 two_hearts
 mark.add_cards(two_hearts)
 print(mark)
+
+# MARK NOW HAS A TOTAL FOR FOUR CARDS
 mark.add_cards([two_hearts,two_hearts,two_hearts])
 print(mark)
+
+# WAR GAME LOGIC
+player_one = Player("One")
+player_two = Player("Two")
+
+# SETUP NEW GAME
+new_deck = Deck()
+new_deck.shuffle()
+
+# SPLIT THE DECK BETWEEN PLAYERS
+print(len(new_deck.all_cards) / 2)
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+print(len(new_deck.all_cards))
+print(len(player_one.all_cards))
+print(len(player_two.all_cards))
